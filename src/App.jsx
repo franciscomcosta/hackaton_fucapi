ffaa9eca8b55aad352c2d819cbca764b9de511cf
unknown <franciscomoreira009@gmail.com>
2023-11-22 22:56:52 -0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Start from './components/StartPage/Start'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Start />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Start />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
