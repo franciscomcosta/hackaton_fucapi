c5e9c69403dbd05f4ef5489df0a0ef21ddf1268b
unknown <franciscomoreira009@gmail.com>
2023-11-22 22:50:48 -0300

    projeto iniciado

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Start from './components/StartPage/Start'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Start />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/StartPage/Start.jsx
@@ -0,0 +1,15 @@
+import Nav from "../nav/Nav"
+
+function Start(){
+    return (
+        <>
+        <section>
+            <h1 className="text-3xl font-bold">
+            Olá
+            </h1>
+        </section>
+        </>
+    )
+}
+
+export default Start
